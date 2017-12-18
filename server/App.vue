8e13b79f69c99fe04960246c7d3a02eb84418bee
holyoak <mericsmith@yahoo.com>
2017-12-18 00:15:05 -0800

    add Vue dev shell

---
+++ server/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Oi Lucas!
+    <oak-checkbox
+      :checked="active"
+      @click="onClickChild">
+    </oak-checkbox>	
+  </div>
+</template>
+
+<script>
+
+export default {
+
+	data () { 
+      return {
+      	active: true
+	}
+
+ },
+
+  methods: {
+    onClickChild (value) {
+      this.active = value
+    }
+  }	
+
+}
+</script>
+
+<style scoped>
+</style>
